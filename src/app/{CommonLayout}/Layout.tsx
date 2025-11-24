4bf2e2464bc6e6eb67ca65af85c6614965b50883
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-11-24 22:30:06 +0600

    install next and shadcn

---
+++ src/app/{CommonLayout}/Layout.tsx
@@ -0,0 +1,12 @@
+import { PublicNavbar } from "@/components/sheared/PublicNavbar";
+
+const CommonLayout = ({ children } : { children: React.ReactNode }) => {
+    return (
+        <>  
+            <PublicNavbar/>
+            {children}
+        </>
+    );
+};
+
+export default CommonLayout;
---
+++ src/components/sheared/PublicNavbar.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+export const PublicNavbar = () => {
+  return (
+    <header>
+        <div><Link href={'/'}>Health Care </Link></div>
+    </header>
+  )
+}
